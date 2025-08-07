3d1a76a2a373ca10ff8cff4f480e8aaf247dd2fc
Marko Živanović <zivanovic.marko@icloud.com>
2025-08-08 00:10:17 +0200

    Minor change

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -5,13 +5,10 @@
 //  Created by Marko Zivanovic on 5. 8. 2025..
 //
 
-import SwiftData
+
 import SwiftUI
 
 struct ContentView: View {
-    
-    @Query var students: [Student]
-    
     var body: some View {
         VStack {
             Image(systemName: "globe")
