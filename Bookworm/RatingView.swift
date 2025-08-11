cd8f25c306cb644f022a3ce2bb0e35591e89ab48
Marko Živanović <zivanovic.marko@icloud.com>
2025-08-11 17:30:13 +0200

    Adding a custom star rating component

---
+++ Bookworm/RatingView.swift
@@ -0,0 +1,54 @@
+//
+//  RatingView.swift
+//  Bookworm
+//
+//  Created by Marko Zivanovic on 11. 8. 2025..
+//
+
+import SwiftUI
+
+struct RatingView: View {
+    
+    @Binding var rating: Int
+    
+    var label = ""
+    var maximumRating = 5
+    
+    var offImage: Image?
+    var onImage = Image(systemName: "star.fill")
+    
+    var offColor = Color.gray
+    var onColor = Color.yellow
+    
+    
+    var body: some View {
+        HStack {
+            if label.isEmpty == false {
+                Text(label)
+            }
+            ForEach(1..<maximumRating + 1, id: \.self) { number in
+                Button {
+                    rating = number
+                } label: {
+                    image(for: number)
+                        .foregroundStyle(number > rating ? offColor : onColor)
+                }
+            }
+        }
+        //MARK: - Preventing whole row can be tapped, just stars
+        .buttonStyle(.plain)
+    }
+    
+    //MARK: - Logic for choosing which image of show
+    func image(for number: Int) -> Image {
+        if number > rating {
+            offImage ?? onImage
+        } else {
+            onImage
+        }
+    }
+}
+
+#Preview {
+    RatingView(rating: .constant(4))
+}
